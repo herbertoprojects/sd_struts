b32125e28a4ea186995506dd0cd0b6ff9bb776c0
Luís Almeida <luis.almeida14799@gmail.com>
2017-12-20 14:50:19 +0000

    Mais

---
+++ src/FrontEnd/user/action/RegisterAction.java
@@ -0,0 +1,12 @@
+package FrontEnd.user.action;
+
+/*import org.apache.struts2.interceptor.SessionAware;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+import FrontEnd.user.model.UserBean;
+
+public class RegisterAction extends ActionSupport implements SessionAware {
+	
+}*/
+
